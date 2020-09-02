12749b03f484dac5e96698ea96f872741caa1c9f
Chaynor Hsiao <chaynorhsiao@gmail.com>
2020-09-01 21:54:57 -0700

    Add form SendCredentials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import SendCredentials from "./SendCredentials";
 
 const App = () => {
   return (
     <div>
       <h1>Jira Bypass</h1>
+      <SendCredentials />
     </div>
   );
 };
